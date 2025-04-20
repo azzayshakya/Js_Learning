46677a59f47d06d546b91cbfcf86fb9cc2dafc5e
Ajay <ajayshakya7376@gmail.com>
2025-04-20 06:24:23 +0530

    learn : practicing js core concepts

---
+++ EXP-1-PRACT.js/index.js
@@ -0,0 +1,50 @@
+console.log("let's get started")
+
+console.log()
+
+if([]){
+    console.log("value si true")
+}else{
+    console.log(" value is false")
+}
+
+
+// 
+// console.log("one")
+// setTimeout(()=>{
+//     console.log("2")
+// },0)
+// console.log("3")
+// Promise.resolve().then(()=>{
+//     console.log("4");
+// })
+// console.log("5")
+
+
+// console.log(2+"5")
+
+// q5
+// console.log(([]+[]))
+
+console.log()
+
+
+console.log(!"hello"); 
+console.log(!!0);      
+console.log(!![]);      
+console.log(!!null);
+
+
+
+// false vs true
+Boolean(undefined)  // false
+Boolean(null)       // false
+Boolean(NaN)        // false
+Boolean(0)          // false
+Boolean('')         // false
+Boolean(false)      // false
+
+Boolean({})         // true
+Boolean([])         // true
+Boolean(' ')        // true
+Boolean('0')        // true
