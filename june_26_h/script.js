460382a935e681528407f6ec03e7e4a55119bf6c
Ajay <ajayshakya7376@gmail.com>
2026-07-29 21:38:45 +0530

    JS more practice

--- june_26_h/script.js
+++ june_26_h/script.js
@@ -33,3 +33,46 @@ const buttonPromise = new Promise((res, rej) => {
   // res("got resoolved on click");
   rej("got rejected on click");
 });
+
+console.log("statitng the questions ");
+// q1
+const newpromo = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("kgot resolved brother");
+  }, 200);
+});
+
+newpromo.then((e) => console.log(e));
+
+//q2
+
+const handleLogin = (id, password) => {
+  // if (!value) return;
+  // console.log("value", value);
+  return new Promise((res, rej) => {
+    if (id == "admin" && password == "admin") {
+      res("u r logged in");
+    } else {
+      rej("wrong id or p ");
+    }
+  });
+};
+handleLogin("admin", "admin")
+  .then((e) => {
+    console.log(e);
+  })
+  .catch((e) => console.log(e));
+
+// q 4
+const fetchData = async (url) => {
+  try {
+    if (url === "/users") {
+      return { id: "1", password: "azzxx" };
+    } else {
+      return { error: "404 not found" };
+    }
+  } catch (error) {}
+};
+
+const result = fetchData("/users");
+console.log("azxx", result);
